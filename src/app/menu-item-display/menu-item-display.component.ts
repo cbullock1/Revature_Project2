1c310dcea26a18ff09e4cdeb0a73013775d51591
Connor Bullock <cbullock1@ufl.edu>
2022-06-11 16:36:10 -0400

    Added the components and begining routing. I exlcuded the app.module and app.component

---
+++ src/app/menu-item-display/menu-item-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item-display',
+  templateUrl: './menu-item-display.component.html',
+  styleUrls: ['./menu-item-display.component.css']
+})
+export class MenuItemDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
