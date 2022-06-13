d1e1c722e9b48ca9b2b30c4e15a88d3e0a8c9b0b
Connor Bullock <cbullock1@ufl.edu>
2022-06-13 00:56:38 -0400

    Reverted before merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './page/home/home.component';
 import { NotfoundComponent } from './page/notfound/notfound.component';
 import { HeaderComponent } from './components/header/header.component';
+=======
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { OrderStatusComponent } from './user/order-status/order-status.component';
+import { OrderHistoryComponent } from './user/order-history/order-history.component';
+import { LogoutComponent } from './user/logout/logout.component';
+import { HttpClientModule  } from '@angular/common/http'
+>>>>>>> parent of 69dfa63... sorted files
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     NavbarComponent,
     HomeComponent,
     NotfoundComponent,
@@ -17,6 +27,20 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule
+=======
+    RegisterComponent,
+    LoginComponent,
+    OrderStatusComponent,
+    OrderHistoryComponent,
+    LogoutComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+>>>>>>> parent of 69dfa63... sorted files
   ],
   providers: [],
   bootstrap: [AppComponent]
