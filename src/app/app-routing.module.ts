69dfa636d7bdadebe8b8eb18deec0e38fcf95e0f
Connor Bullock <cbullock1@ufl.edu>
2022-06-12 21:55:45 -0400

    sorted files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router'
-import { MenuCategoriesComponent } from './menu-categories/menu-categories.component';
-import { MenuPageComponent } from './menu-page/menu-page.component';
+import { MenuCategoriesComponent } from './Components/menu-categories/menu-categories.component';
+import { MenuPageComponent } from './Pages/menu-page/menu-page.component';
+import { LoginComponent } from './user/login/login.component';
+import { LogoutComponent } from './user/logout/logout.component';
+import { OrderHistoryComponent } from './user/order-history/order-history.component';
+import { OrderStatusComponent } from './user/order-status/order-status.component';
+import { RegisterComponent } from './user/register/register.component';
+
 
 const routes : Routes = [
  {path: '', redirectTo:'home', pathMatch:'full'},
  {path:'home', component:MenuCategoriesComponent},
  {path:'menu', component:MenuPageComponent},
- {path:'menu/:menuCatId', component:MenuPageComponent}
+ {path:'menu/:menuCatId', component:MenuPageComponent},
+ {path:'login', component:LoginComponent},
+ {path:'loggout', component:LogoutComponent},
+ {path:'register', component:RegisterComponent},
+ {path:'order-history', component:OrderHistoryComponent},
+ {path:'order-status', component:OrderStatusComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { OrderStatusComponent } from './user/order-status/order-status.component
 import { OrderHistoryComponent } from './user/order-history/order-history.component';
 import { LogoutComponent } from './user/logout/logout.component';
 import { HttpClientModule  } from '@angular/common/http'
+import { MenuCategoriesComponent } from './Components/menu-categories/menu-categories.component';
+import { MenuItemDisplayComponent } from './Components/menu-item-display/menu-item-display.component';
+import { MenuSideBarComponent } from './Components/menu-side-bar/menu-side-bar.component';
+import { MenuPageComponent } from './Pages/menu-page/menu-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -17,13 +22,18 @@ import { HttpClientModule  } from '@angular/common/http'
     OrderStatusComponent,
     OrderHistoryComponent,
     LogoutComponent,
+    MenuCategoriesComponent,
+    MenuItemDisplayComponent,
+    MenuSideBarComponent,
+    MenuPageComponent
     
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
