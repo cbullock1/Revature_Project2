5f374a76d71502f40faa9f75d70b3215575878e6
ttlin030735 <thomaslin030735@gmail.com>
2022-06-12 22:44:30 -0400

    home, navbar and 404

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Project2'`, () => {
+  it(`should have as title 'Revature_Project2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Project2');
+    expect(app.title).toEqual('Revature_Project2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Project2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Revature_Project2 app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './page/home/home.component';
+import { NotfoundComponent } from './page/notfound/notfound.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NotfoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
