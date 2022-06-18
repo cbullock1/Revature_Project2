5a44dea26cda3e11dd2c9ade68b9b1abe6b50a9b
Connor Bullock <cbullock1@ufl.edu>
2022-06-18 16:57:41 -0400

    Added regex and made profile update

--- src/app/components/profile-display/profile-display.component.ts
+++ src/app/components/profile-display/profile-display.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-profile-display',
@@ -7,29 +10,49 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./profile-display.component.css']
 })
 export class ProfileDisplayComponent implements OnInit {
-  public id:string
-  public name:string;
-  public email:string
-  public phone:string
-  public address:string
-  public city:string
-  public state:string
-  public zip:string
+  public Cid:string
+  public Cname:string;
+  public Cemail:string
+  public Cphone:string
+  public Caddress:string
+  public Ccity:string
+  public Cstate:string
+  public Czip:string
   
-  constructor(private cookie: CookieService) {
-    this.name = this.cookie.get("name");
-    this.id = this.cookie.get("userId");
-    this.email = this.cookie.get("email");
-    this.phone = this.cookie.get("phone");
-    this.address = this.cookie.get("address");
-    this.city = this.cookie.get("city");
-    this.state = this.cookie.get("state");
-    this.zip = this.cookie.get("zip");
+  userModel: User;
+  
+  constructor(private cookie: CookieService, private authService: AuthService, private route: Router) {
+    this.Cname = this.cookie.get("name");
+    this.Cid = this.cookie.get("userId");
+    this.Cemail = this.cookie.get("email");
+    this.Cphone = this.cookie.get("phone");
+    this.Caddress = this.cookie.get("address");
+    this.Ccity = this.cookie.get("city");
+    this.Cstate = this.cookie.get("state");
+    this.Czip = this.cookie.get("zip");
+    this.userModel =new User(this.Cid,this.Cname,this.Cemail,this.Cphone, this.Caddress, this.Ccity, this.Cstate, this.Czip);
 
    }
 
   ngOnInit(): void {
     
+  }
+  onSubmitHandler(){
+
+    console.log(this.userModel)
+    this.authService.update(this.userModel).subscribe(response =>{
+      console.log("Update was sucessful!")
+
+      for (const key in response) {
+        if (Object.prototype.hasOwnProperty.call(response, key)) {
+          const element = response[key];
+          this.cookie.set(key, element);
+        }
+      }
+      this.route.navigate(['/home'])
+      
+    })
+
   }
 
 }
