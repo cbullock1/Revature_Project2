ca3399a3f53547801bb307c6907cd7141db4876a
Connor Bullock <cbullock1@ufl.edu>
2022-06-19 18:36:58 -0400

    Outline of order history

--- src/app/components/user/order-history/order-history.component.ts
+++ src/app/components/user/order-history/order-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { DataService } from 'src/app/service/data.service';
 
 @Component({
@@ -7,14 +8,38 @@ import { DataService } from 'src/app/service/data.service';
   styleUrls: ['./order-history.component.css']
 })
 export class OrderHistoryComponent implements OnInit {
- order:any[]=[]
- orderid=1
-  constructor(private dataService:DataService) { }
+
+ foodItemOnOrder: any[] = []
+ orders:any[] = []
+ orderList:any[] = []
+ cartItems:any[] = []
+ cartItemHolder: any[] = [];
+  constructor(private dataService:DataService, private cookie: CookieService) { }
 
   ngOnInit(): void {
-    this.dataService.getOrderByOrderId(this.orderid).subscribe(response =>{
-      this.order=response.data
-
-    })
+    
+    // this.dataService.getOrderByUser(parseInt(this.cookie.get("userId"))).subscribe(response => {
+    //   this.orders = response;
+    //   this.orderList.push(response)
+    //   //console.log(this.orders)
+    //   this.orders.forEach(order => {
+    //     if(order.orderStatus == "complete"){
+    //       this.dataService.getCartById(order.orderId).subscribe(cartResponse => {
+    //         //console.log(cartResponse)
+    //         this.cartItems.push(cartResponse)
+    //         this.cartItemHolder = cartResponse 
+    //         this.cartItemHolder.forEach( cartItem  => {
+    //           this.dataService.getFoodById(cartItem.foodId).subscribe(foods => {
+    //             this.foodItemOnOrder.push(foods)
+    //           })
+    //         })
+    //       })
+    //     }
+    //   })
+    // })
+    // console.log(this.orderList)
+    // console.log(this.cartItems)
+    // console.log(this.foodItemOnOrder)
+    
   }
 }
